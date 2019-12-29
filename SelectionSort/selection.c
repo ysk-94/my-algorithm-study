f491a074e8ef863c5803bd2fcee1d411e49b6043
ysk-94 <00ysk.tech@gmail.com>
2019-12-29 12:54:53 +0900

    SelectionSortのコメント修正

--- SelectionSort/selection.c
+++ SelectionSort/selection.c
@@ -4,13 +4,12 @@ int selectionSort(int array[], int size) {
     int unSortedStart, count, tmp, minimumPointer, sw;
     sw = 0;
 
-    // 先頭から配列の最後の要素までたどる
+    // 入力配列の長さ-1分繰り返す
     // 一つずつソート済み部分が増えていく
     for (unSortedStart = 0; unSortedStart < size - 1; unSortedStart++) {
         // 一旦minimumを初期化
         minimumPointer = unSortedStart;
         // 未ソート部分の先頭から最後の要素までをたどる
-        // なぜsize-1でない??
         for (count = unSortedStart; count < size; count++ ) {
             // もし現在の最小値よりも小さい要素が配列にいたら
             // その要素の位置を最小値ポインタとする
